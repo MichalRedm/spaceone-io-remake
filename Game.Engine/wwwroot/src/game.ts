486295cc7e1f8722b6119796e87d4cceab24a73b
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2026-08-24 21:51:37 +0200

    feat(client): expand camera zoom to 2600 and preserve authentic sprite size ratios

--- Game.Engine/wwwroot/src/game.ts
+++ Game.Engine/wwwroot/src/game.ts
@@ -39,7 +39,7 @@ pixiAny.tilemap = pixiAny.tilemap || pixi_tilemap;
 
 const size = { width: 1000, height: 500 };
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-const zoom = 1050;
+const zoom = 2600;
 const cameraDrag = 0.8;
 
 //PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
