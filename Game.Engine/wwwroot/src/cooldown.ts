ab336b43cc51bd7e43652ddd52186d32d0dd0bd5
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2026-08-24 20:10:13 +0200

    fix(client): hide cooldown bar in spectate mode (#14)

--- Game.Engine/wwwroot/src/cooldown.ts
+++ Game.Engine/wwwroot/src/cooldown.ts
@@ -1,11 +1,26 @@
 import { Settings } from "./settings";
+
 const progress = document.getElementById("cooldown");
 const progressVal = document.getElementById("cooldownValue");
-export class Cooldown {
-  setCooldown(prog: number) {
-    if (Settings.showCooldown) progress.style.visibility = "visible";
-    else progress.style.visibility = "hidden";
 
-    progressVal.style.width = (prog / 255) * 100 + "%";
+export class Cooldown {
+  setCooldown(prog: number): void {
+    if (progress) {
+      if (Settings.showCooldown) {
+        progress.style.visibility = "visible";
+      } else {
+        progress.style.visibility = "hidden";
+      }
+    }
+
+    if (progressVal) {
+      progressVal.style.width = (prog / 255) * 100 + "%";
+    }
+  }
+
+  hide(): void {
+    if (progress) {
+      progress.style.visibility = "hidden";
+    }
   }
 }
